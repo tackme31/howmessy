64c4ae9918f0445dd55dddd92e6e7897da829178
Takumi Yamada <xirtardauq@gmail.com>
2022-03-06 16:12:56 +0900

    v0.1.0

--- Howmessy.VSExtension/Properties/AssemblyInfo.cs
+++ Howmessy.VSExtension/Properties/AssemblyInfo.cs
@@ -13,6 +13,6 @@ using Howmessy.VSExtension;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
-[assembly: AssemblyInformationalVersion("0.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0.0")]
